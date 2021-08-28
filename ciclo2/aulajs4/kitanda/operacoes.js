8716488dd43cac229d03d652dc763e5aada2a14e
aramos197442 <43253198+aramos197442@users.noreply.github.com>
2021-08-28 11:59:39 -0300

    Resolvido o desafio da Aula4 do Ciclo 2 - 28/08/2021

--- ciclo2/aulajs4/kitanda/operacoes.js
+++ ciclo2/aulajs4/kitanda/operacoes.js
@@ -16,6 +16,8 @@ window.onload  = function(){
 	const cestaDoCliente = document.querySelector("ul#cestaDoCliente");
 	const mostraTotalCompra = document.querySelector("#mostraTotalCompra");
 
+	var itemCesta
+
 	var armazenaItens =[];
 	//var armazenaNo = [];
 	var totalPedido = 0;
@@ -26,27 +28,40 @@ window.onload  = function(){
 			li = document.createElement('li');
 			li.className = 'itemCesta'; 
 			li.dataset.precos = item.dataset.precos;
-			//armazenaNo.push(li);
-
-			li.addEventListener('click', (no)=>{
-				console.log(no.firstChild.nodeValue);
-				cestaDoCliente.removeChild(no);
-				totalPedido -= Number(no.dataset.precos);
-				mostraTotalCompra.value = totalPedido.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-			})
 
 			if(armazenaItens.indexOf(item.textContent) == -1){
 				armazenaItens.push(item.textContent);
 
-				cestaDoCliente.appendChild(li).textContent = item.textContent;
+
+				/* Alterei aqui separando a linha "cestaDoCliente.appendChild(li).textContent = item.textContent;" 
+				  em duas linhas criando a variável itemCesta que tem o endereço do Nó a ser removido. */
+				itemCesta = cestaDoCliente.appendChild(li);
+				itemCesta.textContent = item.textContent;
+				/* --- aqui termina a identificação do Nó a ser removido. */
+
+				/* A partir daqui cria-se o evento de retirar o item da cesta de compras e do array armazenaItens 
+				  para que o usuário possa acrescentar na cesta de novo se quiser.Também subtrai o valor do item do
+				  total da compra.*/  
+				itemCesta.addEventListener('click', (foco)=>{
+					armazenaItens.splice(armazenaItens.indexOf(foco.target.textContent),1); //Encontra o item e o exclui do array.
+					cestaDoCliente.removeChild(foco.target); //Remove o Nó (li).
+					totalPedido -= Number(foco.target.dataset.precos); //Subtrai o valor do item excluído do total da compra.
+					mostraTotalCompra.value = totalPedido.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}); //Apresenta o novo valor.
+				});
+				/* Finaliza o evento de exclusão de itens em cesta de compras. */
+
+
 				totalPedido += Number(item.dataset.precos);
 				mostraTotalCompra.value = totalPedido.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
 				
 			}else{
 				alert(`Este item ${item.textContent} já consta de sua cesta`);
 
-			}
-		})
-	})
+			};
+		});
+
+	});
+
 
 }
+
